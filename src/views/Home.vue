d8cc3a1024e7e300490a5e66dfe5aa1e919be181
wangcong <cong_wa@163.com>
2020-11-30 19:57:41 +0800

    fixed(模板):vue3项目初始模板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,11 @@
-<!--
- * 严肃声明：
- * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
- * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
- * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
- * Copyright (c) 2020 陈尼克 all rights reserved.
- * 版权所有，侵权必究！
- *
--->
+
 
 <template>
   <div>
     <header class="home-header wrap" :class="{'active' : headerScroll}">
-      <router-link tag="i" to="./category"><i class="nbicon nbmenu2"></i></router-link>
       <div class="header-search">
         <span class="app-name">新蜂商城</span>
         <i class="iconfont icon-search"></i>
-        <router-link tag="span" class="search-title" to="./product-list?from=home">山河无恙，人间皆安</router-link>
       </div>
       <router-link class="login" tag="span" to="./login" v-if="!isLogin">登录</router-link>
       <router-link class="login" tag="span" to="./user" v-else>
@@ -24,48 +14,8 @@
     </header>
     <nav-bar />
     <swiper :list="swiperList"></swiper>
-    <div class="category-list">
-      <div v-for="item in categoryList" v-bind:key="item.categoryId" @click="tips">
-        <img :src="item.imgUrl">
-        <span>{{item.name}}</span>
-      </div>
-    </div>
-    <div class="good">
-      <header class="good-header">新品上线</header>
-      <div class="good-box">
-        <div class="good-item" v-for="item in newGoodses" :key="item.goodsId" @click="goToDetail(item)">
-          <img :src="$filters.prefix(item.goodsCoverImg)" alt="">
-          <div class="good-desc">
-            <div class="title">{{ item.goodsName }}</div>
-            <div class="price">¥ {{ item.sellingPrice }}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="good">
-      <header class="good-header">热门商品</header>
-      <div class="good-box">
-        <div class="good-item" v-for="item in hots" :key="item.goodsId" @click="goToDetail(item)">
-          <img :src="$filters.prefix(item.goodsCoverImg)" alt="">
-          <div class="good-desc">
-            <div class="title">{{ item.goodsName }}</div>
-            <div class="price">¥ {{ item.sellingPrice }}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="good" :style="{ paddingBottom: '100px'}">
-      <header class="good-header">最新推荐</header>
-      <div class="good-box">
-        <div class="good-item" v-for="item in recommends" :key="item.goodsId" @click="goToDetail(item)">
-          <img :src="$filters.prefix(item.goodsCoverImg)" alt="">
-          <div class="good-desc">
-            <div class="title">{{ item.goodsName }}</div>
-            <div class="price">¥ {{ item.sellingPrice }}</div>
-          </div>
-        </div>
-      </div>
-    </div>
+  
+    
   </div>
 </template>
 
@@ -89,52 +39,6 @@ export default {
       swiperList: [], // 轮播图列表
       isLogin: false, // 是否已登录
       headerScroll: false, // 滚动透明判断
-      hots: [],
-      newGoodses: [],
-      recommends: [],
-      categoryList: [
-        {
-          name: '新蜂超市',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E8%B6%85%E5%B8%82%402x.png',
-          categoryId: 100001
-        }, {
-          name: '新蜂服饰',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E6%9C%8D%E9%A5%B0%402x.png',
-          categoryId: 100003
-        }, {
-          name: '全球购',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E5%85%A8%E7%90%83%E8%B4%AD%402x.png',
-          categoryId: 100002
-        }, {
-          name: '新蜂生鲜',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E7%94%9F%E9%B2%9C%402x.png',
-          categoryId: 100004
-        }, {
-          name: '新蜂到家',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E5%88%B0%E5%AE%B6%402x.png',
-          categoryId: 100005
-        }, {
-          name: '充值缴费',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E5%85%85%E5%80%BC%402x.png',
-          categoryId: 100006
-        }, {
-          name: '9.9元拼',
-          imgUrl: '//s.yezgea02.com/1604041127880/9.9%402x.png',
-          categoryId: 100007
-        }, {
-          name: '领劵',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E9%A2%86%E5%88%B8%402x.png',
-          categoryId: 100008
-        }, {
-          name: '省钱',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E7%9C%81%E9%92%B1%402x.png',
-          categoryId: 100009
-        }, {
-          name: '全部',
-          imgUrl: '//s.yezgea02.com/1604041127880/%E5%85%A8%E9%83%A8%402x.png',
-          categoryId: 100010
-        }
-      ],
     })
     onMounted(async () => {
       const token = getLocal('token')
@@ -147,9 +51,6 @@ export default {
       });
       const { data } = await getHome()
       state.swiperList = data.carousels
-      state.newGoodses = data.newGoodses
-      state.hots = data.hotGoodses
-      state.recommends = data.recommendGoodses
       Toast.clear()
     })
 
